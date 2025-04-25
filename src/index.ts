b012e7ab9bade610164b6771889576915dee2906
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-04-25 22:42:29 +0700

    datatype

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 console.log("Hello, world!");
+
+let x: number | string = 10;
+x = "Hello, TypeScript!";
+if(typeof x === "string") {
+    console.log('x is a string');
+}else  if(typeof x === "number") {
+    console.log('x is a number'); 
+}else {
+    console.log('x is neither a string nor a number');
+}
