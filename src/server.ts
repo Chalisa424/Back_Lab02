85653ae15bbf471e552e9b9ad56ddafd2f86b950
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-04-25 23:07:45 +0700

    send_res_json_obj

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,16 @@ app.get('/',(req:Request, res:Response) =>{
     res.send('Hello World!');
 })
 
+app.get('/test',(req: Request ,res:Response)=>{ 
+    let retunObj ={
+        name: 'test',
+        age: 20,
+        isStudent: true,
+        address: 'New York'       
+    }
+    res.send(retunObj);
+})
+
 app.listen (port,() =>{
     console.log(`App listen at http://localhost:${port}`);
 })
